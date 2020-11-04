d41b970f59a0a2b9925956fbeec5d7190ca0414a
NikLeberg <niklaus.leuenb+github@gmail.com>
2020-11-04 10:41:06 +0100

    better documentation & no computer vs. itself code

--- src/computer/computer.c
+++ src/computer/computer.c
@@ -24,21 +24,19 @@
  * 
  */
 
-typedef struct {
-    uint8_t step;
-    uint8_t startX, startY;
-    direction_t dir;
-    bool foundOnce;
-    uint8_t triedDirections;
-} stateMachine_t;
+// Schrittkette des Suchalgorythmus
+typedef enum {
+    SEARCH_FOR_SHIP = 0,
+    FIRST_HIT,
+    SEARCH_DIRECTION,
+    DIRECTION_FOUND
+} step_t;
 
 
 /**
  * @brief Variablendeklarationen
  * 
  */
-
-static stateMachine_t stateMachine[GROUND_MAX];
 
 
 /**
@@ -48,22 +46,23 @@ static stateMachine_t stateMachine[GROUND_MAX];
 
 /**
  * @brief Erhalte eine pseudo zufällige Koordinate welche noch nicht getroffen wurde und
- *        ähnlich zu einem Schachbrett nur auf Schwartz oder Weiss ist. (per Spiel wird Farbe gewählt)
+ *        ähnlich zu einem Schachbrett nur auf Schwarz oder Weiss ist (per Spiel wird Farbe gewählt).
+ *        Denn das kleinste Schiff ist zwei Felder lang und ist somit immer auf einem schwarzen und weissen Feld.
  * 
  * @param x[out] Pointer auf X Koordinate
  * @param y[out] Pointer auf Y Koordinate
  */
-static void pseudoRandomXY(ground_t ground, uint8_t *x, uint8_t *y);
+static void pseudoRandomXY(uint8_t *x, uint8_t *y);
 
 /**
  * @brief Erhalte eine zufällige Richtung welche noch nicht versucht wurde
  * 
- * @param x[in,out] // toDo
- * @param y[in,out] // toDo
+ * @param x // aktuelle Koordinate X
+ * @param y // aktuelle Koordinate Y
  * @param firstGuess muss true sein wenn die Ausrichtung von einem neuen Schiff gesucht wird
  * @return direction_t eine mögliche Ausrichtung
  */
-static direction_t guessDir(ground_t player, uint8_t x, uint8_t y, bool firstGuess);
+static direction_t guessDir(uint8_t x, uint8_t y, bool firstGuess);
 
 
 /**
@@ -71,48 +70,54 @@ static direction_t guessDir(ground_t player, uint8_t x, uint8_t y, bool firstGue
  * 
  */
 
-void computer(ground_t player, uint8_t *x, uint8_t *y, bool lastWasHit, bool lastHitWasLast) {
-    stateMachine_t *s = &stateMachine[player];
-    if (lastWasHit) ++s->step; // Schrittkette bei Treffer vorwärtsschieben
-    if (lastHitWasLast) s->step = 0; // Schrittkette bei komplett-Abschuss neustarten
-    switch (s->step) {
-        case (0): // zufällige Koordinate auswählen
-            pseudoRandomXY(!player, x, y);
+void computer(uint8_t *x, uint8_t *y, bool lastWasHit, bool lastHitWasLast) {
+    // Zustandsvariablen des Suchalgorythmus
+    static step_t step = SEARCH_FOR_SHIP;
+    static uint8_t startX = SIZE_X, startY = SIZE_Y;
+    static direction_t dir = DIRECTION_MAX;
+    static bool foundOnce = false;
+    if (lastWasHit) ++step; // Schrittkette bei Treffer vorwärtsschieben
+    if (lastHitWasLast) step = SEARCH_FOR_SHIP; // Schrittkette bei komplett-Abschuss neustarten
+    switch (step) {
+        case (SEARCH_FOR_SHIP): // zufällige Koordinate auswählen
+            pseudoRandomXY(x, y);
             break;
-        case (1): // erster Treffer
-            s->startX = *x; // Koordinaten des ersten Treffers speichern
-            s->startY = *y;
-            s->foundOnce = false;
-            s->dir = guessDir(player, *x, *y, true); // schätzen und anhand Koordinatenlage gewisse Richtungen ausschliessen
-            playgroundTranslateCoordinate(x, y, s->dir);
-            s->step = 2;
+        case (FIRST_HIT): // erster Treffer
+            startX = *x; // Koordinaten des ersten Treffers speichern
+            startY = *y;
+            foundOnce = false;
+            dir = guessDir(*x, *y, true); // schätzen und anhand Koordinatenlage gewisse Richtungen ausschliessen
+            playgroundTranslateCoordinate(x, y, dir);
+            step = SEARCH_DIRECTION;
             break;
-        case (2): // Richtung noch nicht gefunden
-            *x = s->startX;
-            *y = s->startY;
-            if (!s->foundOnce) { // wenn zuvor nicht im Schritt 3 gewesen
-                s->dir = guessDir(player, *x, *y, false); // weitere Richtung schätzen
+        case (SEARCH_DIRECTION): // Richtung noch nicht gefunden
+            *x = startX;
+            *y = startY;
+            if (!foundOnce) { // wenn zuvor nicht im Schritt "DIRECTION_FOUND" gewesen
+                dir = guessDir(*x, *y, false); // weitere Richtung schätzen
             } else {
-                // zuvor wurde bereits im Schritt 3 ein Schiffsteil abgeschossen wenn wir anschliessend wieder
-                // hier landen bedeutet dies, dass das Schiff in der gegenüberliegenden Richtung weitergeht
-                s->dir += 2;
-                s->dir %= DIRECTION_MAX;
+                // zuvor wurde bereits im Schritt "DIRECTION_FOUND" ein Schiffsteil abgeschossen wenn wir anschliessend
+                // wieder hier landen bedeutet dies, dass das Schiff in der gegenüberliegenden Richtung weitergeht
+                dir += 2;
+                dir %= DIRECTION_MAX;
             }
-            playgroundTranslateCoordinate(x, y, s->dir);
+            playgroundTranslateCoordinate(x, y, dir);
             break;
-        case (3): // Richtung gefunden, weitermachen
-            s->foundOnce = true;
-            playgroundTranslateCoordinate(x, y, s->dir); // Koordinate in funktionierende Richtung schieben
+        case (DIRECTION_FOUND): // Richtung gefunden, weitermachen
+            foundOnce = true;
+            playgroundTranslateCoordinate(x, y, dir); // Koordinate in funktionierende Richtung schieben
             // wenn nächste Koordinate bereits aufgedeckt, dann stelle Startkoordinate her
             // und suche in die andere Richtung
-            if (playgroundGetState(!player, *x, *y) == FOUND) {
-                *x = s->startX;
-                *y = s->startY;
-                s->dir += 2;
-                s->dir %= DIRECTION_MAX;
-                playgroundTranslateCoordinate(x, y, s->dir);
+            if (playgroundGetState(PLAYER, *x, *y) == FOUND) {
+                *x = startX;
+                *y = startY;
+                dir += 2;
+                dir %= DIRECTION_MAX;
+                playgroundTranslateCoordinate(x, y, dir);
             }
-            s->step = 2;
+            step = SEARCH_DIRECTION; // Setze Schritt um eins zurück. Wenn das neue Ziel ein Treffer war,
+            // dann wird mit ++step zum Beginn der Funktion wieder in diesen Schritt gesprungen. Ansonsten
+            // wird die Richtung umgekehrt.
             break;
     }
 }
@@ -123,48 +128,50 @@ void computer(ground_t player, uint8_t *x, uint8_t *y, bool lastWasHit, bool las
  * 
  */
 
-static void pseudoRandomXY(ground_t ground, uint8_t *x, uint8_t *y) {
-    static uint8_t parity = 3; // pro Spiel einmal eine Parität bestimmen
+static void pseudoRandomXY(uint8_t *x, uint8_t *y) {
+    static uint8_t parity = 3; // pro Spiel einmal eine Parität / Farbe bestimmen
     if (parity == 3) parity = rand() % 2; // 0 - jedes gerade Feld, 1 - jedes ungerade Feld
     bool skip = false;
     do {
         playgroundGetRandomCoordinate(x, y);
-        skip = playgroundGetState(ground, *x, *y) == FOUND; // überspringe bereits aufgedeckte
+        skip = playgroundGetState(PLAYER, *x, *y) == FOUND; // überspringe bereits aufgedeckte
         skip |= *y % 2 == (*x + parity) % 2; // nur jedes zweite Feld relevant (Schachbrettmuster)
-    } while (skip); // überspringe bereits aufgedeckte
+        // wird ermittelt über den Rest der Division der Koordinaten / 2, das ergibt das "Muster": 0 1 0 1 ...
+        // nur wenn der Rest gleich ist (oder bei parity = 1, ungleich) ist die Koordinate "Schachbrettmusterartig".
+    } while (skip);
 }
 
-static direction_t guessDir(ground_t player, uint8_t x, uint8_t y, bool firstGuess) {
-    stateMachine_t *s = &stateMachine[player];
+static direction_t guessDir(uint8_t x, uint8_t y, bool firstGuess) {
+    static uint8_t triedDirections = 0; // speichere bereits versuchte Richtungen
     if (firstGuess) { // bei Neuanfang
-        s->triedDirections = 0; // Versuchte Richtungen zurücksetzen
+        triedDirections = 0; // Versuchte Richtungen zurücksetzen
         // Wenn Koordinate am Rand des Feldes ist, dann kann in dieser Richtung kein Schiff sein
         if (x == 0) {
-            s->triedDirections |= (0x1 << LEFT);
+            triedDirections |= (0x1 << LEFT);
         } else if (x == (SIZE_X - 1)) {
-            s->triedDirections |= (0x1 << RIGHT);
+            triedDirections |= (0x1 << RIGHT);
         }
         if (y == 0) {
-            s->triedDirections |= (0x1 << UP);
+            triedDirections |= (0x1 << UP);
         } else if (y == (SIZE_Y - 1)) {
-            s->triedDirections |= (0x1 << DOWN);
+            triedDirections |= (0x1 << DOWN);
         }
     }
     direction_t dir;
     bool skip = false;
     do {
-        uint8_t copyX = x; // arbeite nur mit der Kopie
+        uint8_t copyX = x; // arbeite nur mit einer Kopie
         uint8_t copyY = y;
         dir = playgroundGetRandomDirection();
         // übergehe Richtung wenn:
-        if ((s->triedDirections >> dir) & 0x1                       // bereits probiert
-        || playgroundTranslateCoordinate(&copyX, &copyY, dir)       // ungültig
-        || playgroundGetState(!player, copyX, copyY) == FOUND) {    // zuvor bereits besucht
+        if ((triedDirections >> dir) & 0x1                      // bereits probiert
+        || playgroundTranslateCoordinate(&copyX, &copyY, dir)   // ungültig
+        || playgroundGetState(PLAYER, copyX, copyY) == FOUND) { // zuvor bereits besucht
             skip = true;
         } else {
             skip = false;
         }
     } while (skip);
-    s->triedDirections |= (0x1 << dir);
+    triedDirections |= (0x1 << dir);
     return dir;
 }
--- src/computer/computer.h
+++ src/computer/computer.h
@@ -40,10 +40,10 @@
 /**
  * @brief Durchläuft den Computeralgorythmus und setzt die Koordinaten auf ein nächstes Ziel
  * 
- * @param x[in,out] Pointer auf die Koordinate X, wird von Fuktion auf neues Ziel gesetzt.
- * @param y[in,out] Pointer auf die Koordinate Y, wird von Fuktion auf neues Ziel gesetzt.
- * @param lastWasHit muss true sein wenn das vorhergehende Ziel nicht ein Leerschuss war, des Computer darf nocheinmal
- *                   schiessen. Die Koordinaten X & Y entsprechen noch dem letzten Treffer
+ * @param x[in,out] Pointer auf die Koordinate X des vorhergehenden Schusses, wird von Funktion auf neues Ziel gesetzt.
+ * @param y[in,out] Pointer auf die Koordinate Y des vorhergehenden Schusses, wird von Funktion auf neues Ziel gesetzt.
+ * @param lastWasHit muss true sein wenn das vorhergehende Ziel nicht ein Leerschuss war, der Computer darf nocheinmal
+ *                   schiessen. Die Koordinaten X & Y müssen noch dem letzten Treffer entsprechen.
  * @param lastHitWasLast muss true sein wenn der vorhergehende Treffer das Schiff versenkt hat.
  */
-void computer(ground_t player, uint8_t *x, uint8_t *y, bool lastWasHit, bool lastHitWasLast);
+void computer(uint8_t *x, uint8_t *y, bool lastWasHit, bool lastHitWasLast);
